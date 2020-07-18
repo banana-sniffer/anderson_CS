ff7e29ddc7e0118c03a920f529b126c170b36af6
William Tai <williamt@berkeley.edu>
2020-07-18 11:18:36 -0700

    Lets make a merge error

--- hello_github.py
+++ hello_github.py
@@ -1,2 +1,12 @@
 print("Hello Github!")
 print("Hello LobsterCheese!")
+
+
+if (1 == 1):
+	print("Hello github again!")
+angela = "playing"
+if (angela == "playing"):
+	print("LOl")
+else:
+	print("bored")
+
